2ed8f2dc6f789030794cf053b86ac1f2f498ee57
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-12-10 21:16:40 +0600

    fix gitlabci file

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -90,7 +90,11 @@ DATABASES = {
         "PASSWORD": env("PASSWORD_DB"),
         "HOST": "postgres",
         "PORT": "5432",
-    }
+    },
+    "test": {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
 }
 
 
