2c3d95d9635ab28e192161448d1d7acbcdc47599
FirstGibon <102861099+erythingIsBusy@users.noreply.github.com>
2023-12-10 20:31:04 +0600

    fix setings

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 env = environ.Env()
 
-DEBUG = env.bool("DEBUG")
+DEBUG = env.bool("DEBUG", default=True)
 
 SECRET_KEY = env("SECRET_KEY")
 
