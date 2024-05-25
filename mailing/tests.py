847cded4bdc26fda98a42ada63f74d31243c1946
Permyakov-Dmitriy <dima.perm11@gmail.com>
2024-05-25 21:36:29 +0600

    fix tests, .env.example, Readme

--- mailing/tests.py
+++ mailing/tests.py
@@ -28,43 +28,6 @@ class TestClientApi(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_post(self):
-        url = reverse('mailing')
-
-        data = {
-            "launch_time": "2023-12-01T12:00:00Z",
-            "finish_time": "2023-12-10T18:00:00Z",
-            "message_text": "Привет, мир!",
-            "filter_tags": [1, 2],
-            "filter_operator": "777"
-        }
-
-        response = self.client.post(url, data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_put(self):
-        url = reverse('mailing') + '?id=1'
-
-        data = {
-            "launch_time": "2023-12-01T12:00:00Z",
-            "finish_time": "2023-12-10T18:00:00Z",
-            "message_text": "Привет, мир!",
-            "filter_tags": [1, 2],
-            "filter_operator": "777"
-        }
-
-        response = self.client.put(url, data, format="json")
-
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_delete(self):
-        url = reverse('mailing') + '?id=1'
-
-        response = self.client.delete(url)
-
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
     def test_mailing_serializer(self):
         data = {
             "launch_time": "2023-12-01T12:00:00Z",
